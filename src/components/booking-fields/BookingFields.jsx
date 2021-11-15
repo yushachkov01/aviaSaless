1a3f391fe8dee88753d869b126d604ee3a0c249c
yushachkov <a_yushachkov@mail.ru>
2021-11-15 13:24:24 +0300

    fetchLocatons и fetchHotels.

--- src/components/booking-fields/BookingFields.jsx
+++ src/components/booking-fields/BookingFields.jsx
@@ -5,6 +5,7 @@ import { useDispatch } from 'react-redux'
 
 function MyField() {
 
+  export function fetchLocations() {
   const dispatch = useDispatch()
 
   const [state, setState] = useState('')
@@ -27,7 +28,8 @@ function MyField() {
 
           return item.dest_id;
         })
-
+        
+        function fetchHotels(){
         destIds.forEach((destId) => {
 
           fetch(`https://booking-com.p.rapidapi.com/v1/hotels/search?dest_type=city&checkin_date=2022-07-24&room_number=1&checkout_date=2022-07-25&order_by=popularity&dest_id=${destId}&adults_number=2&units=metric&filter_by_currency=AED&locale=en-gb&children_ages=5%2C0&include_adjacency=true&page_number=0&categories_filter_ids=class%3A%3A2%2Cclass%3A%3A4%2Cfree_cancellation%3A%3A1&children_number=2`, {
@@ -64,12 +66,16 @@ function MyField() {
   
   return (
     <div className="inputBlock">
-      <input value={state} onChange={(e) => setState(e.target.value)} className="myCity" placeholder="Введите город"></input>
-      <input type="date" className="myInputWhen" placeholder="Когда"></input>
+      <input value={state} onChange={(e) => setState(e.target.value)} className="myCity" style={{backgroundColor: '#f6f6f6', borderRadius:'10px 0px 0px 10px'}} placeholder='Введите город'></input>
+      <input type="date" className="myInputWhen" placeholder="Отъезд"></input>
+      <input type="date" className="myInputWhen" placeholder="Приезд" style={{backgroundColor: '#f6f6f6', borderRadius:'0px 10px 10px 0px'}}></input>
+
 
       <Link to="/results" className="myButtonSaveTickets">Найти отель</Link>
 
     </div>
   )
 }
-export default MyField;
+}
+}
+export default {MyField, FetchLocatons};
